621f7668e0872c382ceecab6f65e27886f0b11b9
valerii6 <markvalonski@gmail.com>
2024-05-24 00:58:27 +0300

    almost finished 1st owl-carousel

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({ 
+    autoplay:true,
+    autoplayTimeout: 10500,
+    autoplaySpeed: 1300,
+    nav: true,
+    navSpeed: 1000,
+    loop: true,
+    responsive:{
+                0:{
+                    items:1
+                },
+            }
+        });
